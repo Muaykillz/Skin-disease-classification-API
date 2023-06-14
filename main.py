d30c4ac886335386eccf84f8ea472da253cf18fa
mukung123 <129653967+Muaykillz@users.noreply.github.com>
2023-06-15 02:44:33 +0700

    heroku5

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, UploadFile, File
-from fastai.vision.core import PILImage
 from PIL import Image
 import io
 import numpy as np
 import pathlib
-from model_helpers import model_meta, _mobilenet_v2_split
 from fastai.vision.all import *
-from torchvision import models
 from torchvision import transforms
 plt = platform.system()
 if plt == 'Windows': pathlib.PosixPath = pathlib.WindowsPath
@@ -16,14 +13,6 @@ app = FastAPI()
 model_path = './model_2.pth'
 model = torch.load(model_path, map_location=torch.device('cpu'))
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
-
-class_names = ['AGEP', 'Angioedema', 'DRESS', 'FDE', 'SJSTEN', 'Urticaria']
-
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
@@ -31,19 +20,31 @@ async def root():
 @app.post("/predict")
 async def image_cls(image: UploadFile = File(...)):
     
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    class_names = ['AGEP', 'Angioedema', 'DRESS', 'FDE', 'SJSTEN', 'Urticaria']
+
     image_data = await image.read()
     img = Image.open(io.BytesIO(image_data))
-    img_array = np.array(img)
+    image_data = None
     img = transform(img).unsqueeze(0)
-    print(img.shape)
-    print(img.dtype)
+    transform = None
+    # print(img.shape)
+    # print(img.dtype)
 
     with torch.no_grad():
         output = model(img)
+
+    img = None
 
     probabilities = torch.softmax(output, dim=1)[0]
     _, predicted_class = torch.max(probabilities, dim=0)
     predicted_label = class_names[predicted_class]
     predicted_probability = probabilities[predicted_class].item()
+    class_names = None
 
     return {"label": f"{predicted_label}", "proba": predicted_probability}
--- test.py
+++ test.py
@@ -1,45 +1,45 @@
-from fastai.vision.all import *
-import torchvision.models as models
-from torchvision import transforms
-import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
-from PIL import Image
+# from fastai.vision.all import *
+# import torchvision.models as models
+# from torchvision import transforms
+# import pathlib
+# temp = pathlib.PosixPath
+# pathlib.PosixPath = pathlib.WindowsPath
+# from PIL import Image
 
-# model = models.mobilenet_v2
+# # model = models.mobilenet_v2
 
-# def _mobilenet_v2_split(m:nn.Module): return L(m[0][0][:7],m[0][0][7:], m[1:]).map(params)
-# _mobilenet_v2_meta = {'cut':-1, 'split':_mobilenet_v2_split, 'stats':imagenet_stats}
-# model_meta[models.mobilenet_v2] = {**_mobilenet_v2_meta}
+# # def _mobilenet_v2_split(m:nn.Module): return L(m[0][0][:7],m[0][0][7:], m[1:]).map(params)
+# # _mobilenet_v2_meta = {'cut':-1, 'split':_mobilenet_v2_split, 'stats':imagenet_stats}
+# # model_meta[models.mobilenet_v2] = {**_mobilenet_v2_meta}
 
-# model = load_learner('./model_1.pkl')
+# # model = load_learner('./model_1.pkl')
 
-model_path = './model_2.pth'
-model = torch.load(model_path, map_location=torch.device('cpu'))
-# model.eval()
+# model_path = './model_2.pth'
+# model = torch.load(model_path, map_location=torch.device('cpu'))
+# # model.eval()
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+# transform = transforms.Compose([
+#     transforms.Resize((224, 224)),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# ])
 
-class_names = ['AGEP', 'Angioedema', 'DRESS', 'FDE', 'SJSTEN', 'Urticaria']
+# class_names = ['AGEP', 'Angioedema', 'DRESS', 'FDE', 'SJSTEN', 'Urticaria']
 
-img = Image.open('./test.jpg')
-img_array = np.array(img)
-print(img_array.shape)
-img = transform(img).unsqueeze(0)
-# print(img)
-print(img.shape)
-print(img.dtype)
-# img_array = PILImage.create(img_array)
-# with torch.no_grad():
-#     output = model(img)
+# img = Image.open('./test.jpg')
+# img_array = np.array(img)
+# print(img_array.shape)
+# img = transform(img).unsqueeze(0)
+# # print(img)
+# print(img.shape)
+# print(img.dtype)
+# # img_array = PILImage.create(img_array)
+# # with torch.no_grad():
+# #     output = model(img)
 
-# probabilities = torch.softmax(output, dim=1)[0]
-# _, predicted_class = torch.max(probabilities, dim=0)
-# predicted_label = class_names[predicted_class]
-# predicted_probability = probabilities[predicted_class].item()
-# print(predicted_label, predicted_probability)
+# # probabilities = torch.softmax(output, dim=1)[0]
+# # _, predicted_class = torch.max(probabilities, dim=0)
+# # predicted_label = class_names[predicted_class]
+# # predicted_probability = probabilities[predicted_class].item()
+# # print(predicted_label, predicted_probability)
 
